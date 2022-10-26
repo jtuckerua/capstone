475372d56fc29cceee10addad0283d26d9554d37
jtucker109 <jtucker109@email.arizona.edu>
2022-10-25 19:12:46 -0700

    Comments
    Andre added comments to his UI additions.

--- my_app/app.py
+++ my_app/app.py
@@ -11,10 +11,14 @@ app_ui = ui.page_fluid(
     ui.layout_sidebar(
         ui.panel_sidebar(
             #Creating different input fields
+            #Numeric input field takes id(str), label, initial value, min, max, step, and width as parameters.
+            #Only the id, label, and initial value are necessary, rest are optional.
             ui.input_numeric("age", "Age", 21, min=1, max=100),
-            ui.input_numeric("fam", "Family_Size", 1, min=1, max=50),
+            ui.input_numeric("fam", "Family Size", 1, min=1, max=50),
             ui.input_numeric("sal", "Salary", 0),
-            #Drop down list of industries
+            #Input select creates a dropdown list of selections.
+            #Parameters: "id" = input id, "label" = input label, "choices" = either a list, dictionary or tuple of choices.
+            #Optionally you can set the initial selection with "selected", and whether multiple selections can be made with "multiple"
             ui.input_select(
                 "industry",
                 "Choose your industry:",
@@ -28,10 +32,16 @@ app_ui = ui.page_fluid(
                 choices=["Buy a home", "Save money","Pay off debt", "Retire"]
             ),
             #Desired distance from current place for moving reccomendations
+            #Input slider allows for a dynamically changing input that can be used in plots etc.
+            #Takes the same parameters as numeric input but you can add a prefix or suffix to the value with "pre" or "post".
             ui.input_slider("dis", "Distance", value=1, min=1, max=1000, step=100, post="mi"),
         ),
+        #panel_main creates the main panel for UI elements to be placed in to the right of the sidebar.
         ui.panel_main(
+            #Row tells shiny that these UI elements should be next to eachother horizontally.
             ui.row(
+                #Column takes a size value telling shiny how much space in each row each element should occupy out of a total of 12.
+                #The other parameter is for ui elements to place within the column.
                 ui.column(6, output_widget("map")),
                 ui.column(6, ui.output_plot("plot")),
             ),
@@ -50,8 +60,10 @@ def server(input, output, session):
     # Initialize map
     map = L.Map(center=(32.2540,-110.9742), zoom=12, scroll_wheel_zoom=True)
     register_widget("map", map)
+    #output and render.plot need to be called before every plot for it to load.
     @output
     @render.plot(alt="Test")
+    #These are currently just placeholder plots that are tied to the input_slider.
     def plot():
         np.random.seed(19680001)
         x = 100 + 15 * np.random.randn(437)
