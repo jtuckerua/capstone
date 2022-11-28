0a9ca5ae8d829afc8c26abf48e5d85aba1477fc9
jtucker109 <jtucker109@email.arizona.edu>
2022-11-28 15:33:06 -0700

    Elijah updates
    He made updates to the frontend.

--- my_app/app.py
+++ my_app/app.py
@@ -41,8 +41,12 @@ app_ui = ui.page_fluid(
        'Arts, entertainment, and recreation','Accommodation and food services','Other services, except public administration','Public administration', 'Unclassified'], width='20%'),
         ui.input_numeric("sal", "Salary", 10000, min=10000, max=1000000, width='10%'),
         ui.input_numeric("sav", "Savings", 0, min=0, max=1000000, width='10%'),
+    ),
+    ui.row(
         ui.input_numeric("age", "Age", 18, min=1, max=100, width='10%'),
         ui.input_numeric("fam", "Family #", 1, min=1, max=10, width='10%'),
+        ui.input_numeric("zip", "Current Zipcode", 0, min=10000, max=99999, width='10%'),
+        ui.input_numeric("rent", "Rent", 0, min=0, max=10000, width='10%'),
         ui.input_slider("dis", "Distance", value=1, min=1, max=1000, step=50, post="mi", width='20%'),
         ui.input_action_button("predict","Predict", width='10%'),
         ui.output_text_verbatim("results", placeholder=True)
@@ -63,20 +67,33 @@ def server(input, output, session):
         industry = input.industry
         salary = input.sal
         savings = input.sav
-        age = input.age
-        family = input.fam
-        distance = input.dis
+        rent = input.rent
+        location = input.loc
         debt = input.debt
         # Run calculations
-        results = calcs.calculations([salary, savings, debt, goal, 0, 0, industry])
+        results = calcs.calculations([salary, savings, debt, goal, rent, location, industry])
         # Return results
         return results
     # This function is used to display data that is returned from the db
     async def results():
-        vals = [input.age(), input.fam(), input.sal()]
-        # print(calcs.calc_values(vals))
-        await asyncio.sleep(2)
-        return f"Age: {input.age()}, Family: {input.fam()}, Salary: {input.sal()}"
+        # get output from calculations
+        results = await predict()
+        # display results
+        return results
+
+    @reactive.event(input.predict)
+    # change city A to the city returned by the results fuction
+    @output("page_navbar: tab a content")
+    @render.text
+    async def city_a():
+        return await results()
+
+    @reactive.event(input.predict)
+    # change city B to the city returned by the results fuction
+    @output("page_navbar: tab b content")
+    @render.text
+    async def city_b():
+        return await results()
     #output and render.plot need to be called before every plot for it to load.
     @output
     @render.plot(alt="Test")
