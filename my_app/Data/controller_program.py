26549236a778f589367ef3536905c756f63ec18d
jtucker109 <jtucker109@email.arizona.edu>
2022-11-29 20:07:42 -0700

    controller program

--- my_app/Data/controller_program.py
+++ my_app/Data/controller_program.py
@@ -1,33 +1,58 @@
 import pandas as pd
 from calculations import *
 from location import *
-
+industries = { 'Total, all industries': '10',
+                'Agriculture, forestry, fishing and hunting':'11',
+                'Mining, quarrying, and oil and gas extraction':'21',
+                'Utilities':'22',
+                'Construction':'23',
+                'Manufacturing':'31-33',
+                'Wholesale trade':'42',
+                'Retail trade':'44-45',
+                'Transportation and warehousing':'48-49',
+                'Information':'51',
+                'Finance and insurance':'52',
+                'Real estate and rental and leasing':'53',
+                'Professional and technical services':'54',
+                'Management of companies and enterprises':'55',
+                'Administrative and waste services':'56',
+                'Educational services':'61',
+                'Health care and social assistance':'62',
+                'Arts, entertainment, and recreation':'71',
+                'Accommodation and food services':'72',
+                'Other services, except public administration':'81',
+                'Public administration':'92',
+                'Unclassified':'99'}
 
 def calculations(data):
-    """
-    Salary = data[0]
-    savings = data[1]
-    debt = data[2]
-    goal = data[3]
-    rent = data[4]
-    bedroom = data[5]
-    location = data[6]
-    distance = data[7]
-    industry = data[8]
-    """
-    # retval = {
-    #     'current location' :[location.location_info(data[6]), d_income(data[0], data[4])],
-    #     'location 1' : [],
-    #     'location 2': [],
-    #     'location 3' : []
-    # }
+  """
+  Salary = data[0]
+  savings = data[1]
+  debt = data[2]
+  goal = data[3]
+  rent = data[4]
+  bedroom = data[5]
+  location = data[6]
+  distance = data[7]
+  industry = data[8]
+  """
+  ind_df = get_industry_df(industries.get(str(data[8])))
 
-def get_industry(industry):
+
+def get_industry_df(industry):
   """
   collect all rows based on industry
   """
   df = pd.read_csv('./Clean/location_industry.csv')
-  codes = df['industry_code'].unique()
-  fips = df[df['industry_code'] == codes[4]].sort_values('annual_avg_emplvl', axis=0, ascending=False)[:5]['area_fips'].values
-  lf = pd.read_csv('/Users/jasontucker/Desktop/School/Fall 2022/Ista 498/Capstone/Project/Untitled/my_app/Data/Clean/location_codes.csv')
-  rets = lf[lf['area_fips'].isin(fips)].values
+  # codes = df['industry_code'].unique()
+  return df[df['industry_code'] == industry].sort_values('annual_avg_emplvl', axis=0, ascending=False)[:10]['area_fips']
+  
+def get_industry_loc(fips):
+  lf = pd.read_csv('./Clean/location_codes.csv')
+  return lf[lf['area_fips'].isin(fips)].loc[:,['City','State']]
+
+def get_rent(data):
+  locs = []
+  for line in data:
+
+    location.location_info()
