9b129516e3b0c503f11b4a7ce837ec02428892f1
eliacuna <elijahacuna@email.arizona.edu>
2022-12-02 15:02:03 -0700

    fixing var name bugs

--- my_app/app.py
+++ my_app/app.py
@@ -87,7 +87,7 @@ def server(input, output, session):
             debt.append((input[f"pay{i}"], input[f"int{i}"], input[f"term{i}"]))
         # Get data from database
         # Run calculations
-        results = ctl.calcs([input.salary, input.savings, debt, input.goal, input.rent, input.rooms, input.location, input.industry])
+        results = ctl.calcs([input.sal, input.sav, debt, input.goal, input.rent, input.bedrooms, input.location, input.industry])
 
         # get the current tab
         tab = input.tab()
