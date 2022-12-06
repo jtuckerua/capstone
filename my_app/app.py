ac5303bff9ca587bde3e6a1dfea7cd0b830c5d5c
eliacuna <elijahacuna@email.arizona.edu>
2022-12-05 18:10:50 -0700

    major update to plots

--- my_app/app.py
+++ my_app/app.py
@@ -1,4 +1,6 @@
+import difflib
 from logging import PlaceHolder
+import math
 from tkinter import CENTER
 from turtle import width
 from shiny import App, render, ui, reactive, Outputs
@@ -9,11 +11,11 @@ import matplotlib.pyplot as plt
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 import numpy as np
-import seaborn as sns
 import pandas as pd
+import seaborn as sb
 
-sns.set_theme()
-
+# show all dataframe columns
+pd.set_option('display.max_columns', None)
 
 def nav_controls(prefix):
     return [
@@ -22,6 +24,12 @@ def nav_controls(prefix):
         ui.nav("City C"),
     ]
 
+###### Load data ######
+wages_df = pd.read_csv('./Data/Clean/cln_wages.csv')
+rent_df = pd.read_csv('./Data/Clean/rent.csv')
+ind_series = wages_df['OCC_TITLE'].values.tolist()
+
+###### Create App ######
     
 app_ui = ui.page_fluid(
     # create a user interface that features a horizontal navbar that contains a title and a button
@@ -40,17 +48,14 @@ app_ui = ui.page_fluid(
     ))),
     ui.row(
         ui.input_select("goal", "Financial Goal", ["Buy a home", "Improve Quality of Life","Investment Property"], width='20%'),
-        ui.input_select("industry", "Job Industry", ['Total, all industries','Agriculture, forestry, fishing and hunting','Mining, quarrying, and oil and gas extraction', 'Utilities',
-       'Construction', 'Manufacturing', 'Wholesale trade', 'Retail trade','Transportation and warehousing', 'Information','Finance and insurance', 'Real estate and rental and leasing',
-       'Professional and technical services','Management of companies and enterprises','Administrative and waste services', 'Educational services','Health care and social assistance',
-       'Arts, entertainment, and recreation','Accommodation and food services','Other services, except public administration','Public administration', 'Unclassified'], width='20%'),
+        ui.input_select("industry", "Job Industry", ind_series, width='20%'),
         ui.input_numeric("sal", "Salary", 10000, min=0, max=1000000, width='20%'),
         ui.input_numeric("sav", "Savings", 0, min=0, max=1000000, width='20%'),
         ui.input_numeric("age", "Age", 18, min=1, max=100, width='10%'),
     ),
     ui.row(
-        ui.input_numeric("zip", "Current Zip Code", 0, min=10000, max=99999, width='20%'),
-        ui.input_numeric("rent", "Rent", 500, min=0, max=10000, width='20%'),
+        ui.input_numeric("zip", "Current Zip Code", 10001, min=10000, max=99999, width='20%'),
+        ui.input_numeric("rent", "Rent", 945, min=0, max=10000, width='20%'),
         ui.input_select("bedrooms", "Number of Bedrooms", ["Studio", "1BR", "2BR", "3BR", "4BR"], width='20%'),
         ui.input_slider("dis", "Distance", value=1, min=1, max=500, step=50, post="mi", width='20%'),
         ui.input_checkbox_group("checkbox_item", "Nationwide?", choices=["Yes"], width='10%'),
@@ -65,9 +70,9 @@ app_ui = ui.page_fluid(
         
 
 def server(input, output, session):
-    ###### Load data ######
-    wages_df = pd.read_csv('./Data/Clean/cln_wages.csv')
-    rent_df = pd.read_csv('./Data/Clean/rent.csv')
+    
+    # extract 'ZIP Code', 'City', 'State' from wages_df
+    zip_df = rent_df[['ZIP Code', 'City', 'State']]
 
     ###### Calc Functions ######
     @reactive.Calc
@@ -75,27 +80,34 @@ def server(input, output, session):
         geolocator = Nominatim(user_agent="my_app")
         location1 = geolocator.geocode(zip1)
         location2 = geolocator.geocode(zip2)
-        return geodesic((location1.latitude, location1.longitude), (location2.latitude, location2.longitude)).miles
+        miles = geodesic((location1.latitude, location1.longitude), (location2.latitude, location2.longitude)).miles
+        if miles <= input.dis():
+            return [zip2]
+        else:
+            return []
 
     @reactive.Calc
-    def filter_rent_df():
-        # if the user selects the nationwide checkbox, then the rent dataframe
-        # will not be filtered by location
-        if input.checkbox_item:
-            return rent_df
-        # if the user does not select the nationwide checkbox, then the rent dataframe
-        # will be filtered by location to only include the zip code that the user inputs
-        # and all the zip codes that are within the distance selected by the user.
-        else:
-            # use nested loops to make a list of all the zip codes that are within the distance
-            # selected by the user from the inputted zip code.
-            zip_list = []
-            for i in range(len(rent_df)):
-                if determine_distance_by_zipcode(input.zip(), rent_df['ZIP Code'][i]) <= input.dis():
-                    zip_list.append(rent_df['ZIP Code'][i])
-            # filter the rent dataframe by the list of zip codes that are within the distance
-            # selected by the user from the inputted zip code.
-            return rent_df[rent_df['ZIP Code'].isin(zip_list)]
+    def get_city_state_from_zip():
+        zip = input.zip()
+        geolocator = Nominatim(user_agent="my_app")
+        location = geolocator.geocode(zip, addressdetails=True)
+        location = location.raw
+        city = location['address']['city']
+        state = location['address']['state']
+
+        df = wages_df[['City', 'State']]
+
+        if city.startswith('City of '):
+            city = city.replace('City of ', '')
+
+        # find the first row where the zip_df['City'].contains city and zip_df['State'] == state
+        for ind in df.index:
+            if city in df['City'][ind]:
+                print("CITY:", city, "=>", df['City'][ind])
+                city = df['City'][ind]
+                state = df['State'][ind]
+                break
+        return city, state
 
     @reactive.Calc
     def calc_estimated_rent():
@@ -108,22 +120,44 @@ def server(input, output, session):
         in the filtered data. It will then select the lowest cost city and
         return the estimated rent for that city.
         '''
-        # get localized rent_df from user input
-        rent_df_local = filter_rent_df()
-        # filter the rent_df_local to the bedroom size the user selected with the input var directly
-        rent_df_local = rent_df_local[input.bedrooms().lower()]
-        # get the mean cost for each zip code in the filtered data
-        rent_df_local = rent_df_local.groupby(rent_df_local.index).mean()
+        bedrooms = input.bedrooms()
+        if bedrooms == 'Studio':
+            bedrooms = 'studio'
 
-        # if the current tab is 'City A', then return the estimated rent for the lowest cost city
-        if input.tab() == 'City A':
-            return rent_df_local.min()
-        # if the current tab is 'City B', then return the estimated rent for the second lowest cost city
-        elif input.tab() == 'City B':
-            return rent_df_local.nsmallest(2).max()
-        # if the current tab is 'City C', then return the estimated rent for the third lowest cost city
-        elif input.tab() == 'City C':
-            return rent_df_local.nsmallest(3).max()
+        # get the 'City' value for the input zip code
+        zip = input.zip()
+        city = zip_df.loc[zip_df['ZIP Code'] == int(zip)]['City'].values[0]
+
+        # filter the rent data to only be the [bedroom] column where the 'City' == city
+        rent = rent_df[rent_df['City'] == city][bedrooms]
+
+        mean = round(rent.mean(), 2)
+        return mean
+
+    @reactive.Calc
+    def calc_estimated_salary():
+        '''
+        This function will calculate the estimated rent based on the 
+        mean cost for their selected number of bedrooms in the rent_df
+        subset that their input filters for.
+        It will first filter the data to the bedroom size they selected
+        and then get the mean cost for that bedroom size for each zip code
+        in the filtered data. It will then select the lowest cost city and
+        return the estimated rent for that city.
+        '''
+        bedrooms = input.bedrooms()
+        if bedrooms == 'Studio':
+            bedrooms = 'studio'
+
+        # get the 'City' value for the input zip code
+        city, state = get_city_state_from_zip()
+
+        # filter the rent data to only be the [bedroom] column where the 'City' == city
+        wages = wages_df[wages_df['City'] == city]
+        wages = wages[wages['State'] == state]
+        wages = wages[wages['OCC_TITLE'] == input.industry()]
+
+        return wages['Mean_Annual_wage'].values[0]
 
     ###### Update UI ######
     @reactive.Effect
@@ -134,11 +168,9 @@ def server(input, output, session):
         Initialize the map with coordinates to Tucson, AZ.
         Will also update the map when the predictions are made
         '''
-        print("ZIP:", input.zip())
         geolocator = Nominatim(user_agent="my_app")
         location = geolocator.geocode(input.zip())
-        if location == None:
-            location = geolocator.geocode(85701)
+        
         lat = location.latitude
         lon = location.longitude
 
@@ -156,15 +188,33 @@ def server(input, output, session):
         '''
         This function will create a plot that will show the average salary for the user's specific calculations
         '''
-        fig, ax = plt.subplots()
+        print(input.industry())
 
-        # use an sns histplot to create a histogram
-        # filter wages_df by the industry selected in the dropdown menu
+        zip = input.zip()
+        city = zip_df.loc[zip_df['ZIP Code'] == int(zip)]['City'].values[0]
+
+        nat_wages = wages_df[wages_df['OCC_TITLE'] == input.industry()]
+        nat_wages = nat_wages['Mean_Annual_wage']
+
+        # get the mean annual wage for the selected industry
         wages = wages_df[wages_df['OCC_TITLE'] == input.industry()]
-        sns.histplot(wages, ax=ax, kde=True, bins=20)
-        print("updating plot...")
-        return fig
+        wages = wages_df[wages_df['City'] == city]
+        wages = wages['Mean_Annual_wage']
 
+        if len(wages) >= len(nat_wages):
+            wages = wages[:len(nat_wages)]
+        else:
+            nat_wages = nat_wages[:len(wages)]
+
+        # create the plot
+        fig, ax = plt.subplots()
+        ax.hist(wages, bins=10, alpha=0.5, label=city)
+        ax.hist(nat_wages, bins=10, alpha=0.5, label='Nationwide')
+        ax.set_xlabel('City')
+        ax.set_ylabel('Frequency')
+        ax.set_title('Average Salary for ' + input.industry())
+        ax.legend(loc='upper right')
+        return fig
 
     @reactive.Effect
     @output
@@ -173,16 +223,26 @@ def server(input, output, session):
         '''
         This function will create a plot that will show the average rent for the user's specific calculations
         '''
+        rent = rent_df
         zip = str(input.zip())
-        geolocator = Nominatim(user_agent="my_app")
-        location = geolocator.geocode(zip)
-        city = location
+        
+        # get the 'City' value for the input zip code
+        city = zip_df.loc[zip_df['ZIP Code'] == int(zip)]['City'].values[0]
+
+        bedrooms = input.bedrooms()
+        if bedrooms == 'Studio':
+            bedrooms = 'studio'
+
+        # filter the rent data to only be the [bedroom] column where the 'City' == city
+        rent = rent_df.loc[rent_df['City'] == city]
+        rent = rent[bedrooms]
 
         fig, ax = plt.subplots()
-        sns.histplot(rent_df, ax=ax, kde=True, bins=20)
-        print("updating plot_2...")
-        return fig
-
+        ax.hist(rent, bins=10)
+        ax.set_xlabel('City')
+        ax.set_ylabel('Rent')
+        ax.set_title('Rent for ' + bedrooms + ' in ' + city)
+        
     @reactive.Effect
     @output
     @render.text
@@ -191,15 +251,14 @@ def server(input, output, session):
         This function will create a text box that will show the user's specific calculations
         '''
         zip = str(input.zip())
-        geolocator = Nominatim(user_agent="my_app")
-        location = geolocator.geocode(zip)
-        city = location
-        print("city:", city)
-        return  f"Ideal City: {city}\n" + \
-                f"Estimated Salary: {input.sal()}\n" + \
-                f"Estimated Rent: {calc_estimated_rent()} {input.bedrooms()} \n" + \
-                f"Estimated Annual Savings: {input.sav()} (Assuming 10% of Disposable Income)\n" + \
-                f"Estimated Disposable Income: {input.sal()} Per Month\n"
+        # get the 'City' value for the input zip code
+        city = zip_df.loc[zip_df['ZIP Code'] == int(zip)]['City'].values[0]
+        state = zip_df.loc[zip_df['ZIP Code'] == int(zip)]['State'].values[0]
+        return  f"Ideal City: {city}, {state}\n"+ \
+                f"Estimated Salary: ${calc_estimated_salary()}\n" + \
+                f"Estimated Rent: ${calc_estimated_rent()} {input.bedrooms()} \n" + \
+                f"Estimated Annual Savings: ${input.sav()} (Assuming 10% of Disposable Income)\n" + \
+                f"Estimated Disposable Income: ${input.sal()} Per Month\n"
 
 
 app = App(app_ui, server)
